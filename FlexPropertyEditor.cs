69574304a4efcc5af2f48a971d9a45baf8ffb848
Daniël Knippers <daniel.knippers@perplex.nl>
2021-12-19 13:36:32 +0100

    Change target framework to net5.0

--- FlexPropertyEditor.cs
+++ FlexPropertyEditor.cs
@@ -1,13 +1,14 @@
 using Umbraco.Cms.Core.PropertyEditors;
-namespace DF21;
-
-[DataEditor(
-    name: "Flex",
-    alias: "Perplex.Flex",
-    view: "/App_Plugins/Perplex.Flex/perplex.flex.html",
-    ValueType = ValueTypes.Json
-)]
-public class FlexPropertyEditor : DataEditor
+namespace DF21
 {
-    public FlexPropertyEditor(IDataValueEditorFactory dataValueEditorFactory) : base(dataValueEditorFactory) { }
+    [DataEditor(
+        name: "Flex",
+        alias: "Perplex.Flex",
+        view: "/App_Plugins/Perplex.Flex/perplex.flex.html",
+        ValueType = ValueTypes.Json
+    )]
+    public class FlexPropertyEditor : DataEditor
+    {
+        public FlexPropertyEditor(IDataValueEditorFactory dataValueEditorFactory) : base(dataValueEditorFactory) { }
+    }
 }
